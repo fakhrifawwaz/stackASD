19683c2e3589a47bb1c1e460e4c9994f66688145
fakhrifawwaz <fakhri07fawwaz@gmail.com>
2026-04-24 12:59:46 +0700

    tugas1

---
+++ Surat15.java
@@ -0,0 +1,20 @@
+public class Surat15 {
+    
+    String idSurat;
+    String namaMahasiswa;
+    String kelas;
+    char jenisIzin;
+    int durasi;
+
+    Surat15() {
+    }
+
+    Surat15(String idSurat, String namaMahasiswa, String kelas, char jenisIzin, int durasi) {
+        this.idSurat = idSurat;
+        this.namaMahasiswa = namaMahasiswa;
+        this.kelas = kelas;
+        this.durasi = durasi;
+
+    }
+
+}
